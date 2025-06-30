3152a305d1773aa037098200d00df902e7cdb465
phan <hanfeelhoo@naver.com>
2025-06-30 21:43:25 +0900

    AStar class and Node class init

--- planning/Astar/AStar.py
+++ planning/Astar/AStar.py
@@ -1,6 +1,24 @@
 import math
 import matplotlib.pyplot as plt
 
+class Node:
+    def __init__(self, x, y, cost, p_idx=-1):
+        self.x = x
+        self.y = y
+        self.cost = cost
+        self.p_idx = p_idx # parent index
+
+    def __str__(self):
+        return f"x: {self.x}, y: {self.y}, cost: {self.cost}, p_idx: {self.p_idx}"
+
+
 class AStar:
     def __init__(self):
         pass
+
+    def planning(self, start_pos, goal_pos):
+        pass
+
+if __name__ == "__main__":
+    a_star = AStar()
+    a_star.planning((0, 0), (10, 10))
