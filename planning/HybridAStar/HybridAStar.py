3a6883e66e8006859a55d1277537a0341d573384
phan <hanfeelhoo@naver.com>
2025-07-17 14:50:47 +0900

    hybrid a*

--- planning/HybridAStar/HybridAStar.py
+++ planning/HybridAStar/HybridAStar.py
@@ -1,38 +1,8 @@
-'''
-TODO
-- [] Node 정의
-    - [] x, y, yaw, r (= forward 1, backward 0), cost, parent_idx
-- [] Map 정의
-    - [] 장애물 정보, 맴 크기, 해상도
-- [] HybridAStar 정의
-    - [] 맵, 자동차, 시작점, 도착점 (방향까지)
-    - [] expansion 방식 정의
-    - [] heuristic fucntion
-        - [] non-holonomic-without-obstacle (reeds-shepp action)
-            - [] 4D 정보 사용 (x, y, ywa, r)
-            - [] 시작 초기 한번에 다 계산해두면 됨
-        - [] holonomic-with-obstacle (dp - dijkstra)
-            - [] 2D 정보 사용 (x, y)
-        - [] 둘중에 큰걸 사용
-    - [] analytic expansions
-        - [] reeds-shepp를 이용해서 탐색속도 향상 시킴
-            - [] 장애물 맵에 대해 충돌이 없는 것이 확인되면 해당 경로가 자식 노드로 추가됨.
-            - [] 매번 계산을 하게되면 비용이 많이 들기 때문에 매 N번째 노드일때 수행 (목표에 가까워 질 수록 자주 분석적 확장을 하게됨.)
-
-    - [] planning
-'''
-
 import math
-import heapq
-import itertools
-
-
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.spatial import cKDTree
-from pprint import pprint
-from Car import Car
 
+from Car import Car
 class Node:
     def __init__(self, x, y, yaw, x_list, y_list, yaw_list, r=1, cost=0.0, p_idx=-1):
         self.x = x
@@ -100,10 +70,14 @@ class Map:
         y = node.y * resolution + self.min_y
         return self.obstacle_map[x][y]
 
-    def get_grid_idx(self, cx, cy, resolution):
+    def get_grid2D_idx(self, cx, cy, resolution):
         width = round(self.x_width / resolution)
-        # height = round((self.max_y - self.min_y) / resolution)
-        return round((cy - self.min_y) * width + (cx - self.min_x))
+        return round(cy * width + cx)
+
+    def get_grid3D_idx(self, cx, cy, cyaw, resolution):
+        width = round(self.x_width / resolution)
+        height = round(self.y_width / resolution)
+        return round(cx + cy * width + cyaw * width * height)
 
 
     @staticmethod
@@ -133,18 +107,14 @@ class HybridAStar:
                          round((self.goal[1] - self.map.min_y) / self.xy_resolution),
                          round(self.goal[2] / self.yaw_resolution),
                          [self.goal[0]], [self.goal[1]], [self.goal[2]])
+        
         open_set = {}
         closed_set = {}
 
         self.non_holonomic_table = self.non_holonomic_without_obstacles(start_node, goal_node)
-        # pprint(self.non_holonomic_table, sort_dicts=False)
         self.holonomic_table = self.holonomic_with_obstacles(start_node, goal_node)
-        pprint(self.holonomic_table, sort_dicts=True)
 
-        map_width_reso = round(self.map.x_width / self.xy_resolution)
-        map_height_reso = round(self.map.y_width / self.xy_resolution)
-
-        start_id = start_node.x + start_node.y * map_width_reso + start_node.yaw * map_width_reso * map_height_reso
+        start_id = self.map.get_grid3D_idx(start_node.x, start_node.y, start_node.yaw, self.xy_resolution)
         open_set[start_id] = start_node
 
         while open_set:
@@ -152,10 +122,7 @@ class HybridAStar:
             curr = open_set[curr_id]
 
             self.vehicle.x, self.vehicle.y, self.vehicle.yaw = curr.x * self.xy_resolution, curr.y * self.xy_resolution, curr.yaw * self.yaw_resolution
-            # self.vehicle.draw()
-            # print(curr.yaw_list)
             self.vehicle.display_arrow("green")
-            # plt.plot(curr.x, curr.y, "xc")
             if len(closed_set.keys()) % 1000 == 0:
                 plt.pause(0.001)
 
@@ -178,7 +145,6 @@ class HybridAStar:
                 if next_id in closed_set and closed_set[next_id].cost > next_node.cost:
                     del closed_set[next_id]
 
-                # print(next_node.x_list)
                 if next_id in open_set and open_set[next_id].cost > next_node.cost:
                     del open_set[next_id]
 
@@ -188,12 +154,10 @@ class HybridAStar:
         return goal_node.x_list, goal_node.y_list, goal_node.yaw_list
 
     def get_next_node(self, curr_id, curr, goal_node, angle, direction):
-        # Penalty
+
         reverse_penalty = 10.0
         change_direction_penalty = 30.0
         steering_penalty = 5
-        map_width_reso = round(self.map.x_width / self.xy_resolution)
-        map_height_reso = round(self.map.y_width / self.xy_resolution)
 
         nx, ny, nyaw = self.vehicle.action(curr.x_list[-1], curr.y_list[-1], curr.yaw_list[-1], angle, direction)
 
@@ -215,8 +179,15 @@ class HybridAStar:
         new_cost += abs(angle) * steering_penalty
         new_cost += abs(curr.yaw - nyaw) * steering_penalty
 
-        next_node_id_2d = round(nx / self.xy_resolution) + round(ny / self.xy_resolution) * map_width_reso
-        next_node_id = next_node_id_2d + round(nyaw / self.yaw_resolution) * map_width_reso * map_height_reso
+        next_node_id_2d = self.map.get_grid2D_idx(
+                            round(nx / self.xy_resolution),
+                            round(ny / self.xy_resolution),
+                            self.xy_resolution)
+        next_node_id = self.map.get_grid3D_idx(
+                            round(nx / self.xy_resolution),
+                            round(ny / self.xy_resolution),
+                            round(nyaw / self.yaw_resolution),
+                            self.xy_resolution)
 
         if next_node_id not in self.non_holonomic_table:
             self.non_holonomic_table[next_node_id] = 0.0
@@ -224,14 +195,12 @@ class HybridAStar:
             self.holonomic_table[next_node_id_2d] = 0.0
 
         new_cost += max(self.non_holonomic_table[next_node_id], self.holonomic_table[next_node_id_2d])
-        # print(new_cost)
 
         next_node = Node(round(nx / self.xy_resolution), round(ny / self.xy_resolution), round(nyaw / self.yaw_resolution),
                          curr.x_list[:] + [nx],
                          curr.y_list[:] + [ny],
                          curr.yaw_list[:] + [nyaw],
                          direction, new_cost, curr_id)
-        # print(next_node_id)
         return next_node_id, next_node
 
 
@@ -246,15 +215,12 @@ class HybridAStar:
         reverse_penalty = 10.0
         change_direction_penalty = 50.0
         steering_penalty = 1.5
-        move_cost = 1.0
-        map_width_reso = round(self.map.x_width / self.xy_resolution)
-        map_height_reso = round(self.map.y_width / self.xy_resolution)
 
         open_set = {} # key: (x, y)
         curr_pos = {}
         closed_set = {}
 
-        curr_idx = start_node.x + start_node.y * self.map.x_width + start_node.yaw * self.map.x_width * self.map.y_width
+        curr_idx = self.map.get_grid3D_idx(start_node.x, start_node.y, start_node.yaw, self.xy_resolution)
         curr_pos[curr_idx] = [start_node.x_list[-1], start_node.y_list[-1], start_node.yaw_list[-1], start_node.r]
 
         open_set[curr_idx] = 0.0
@@ -283,20 +249,17 @@ class HybridAStar:
                         and (self.map.min_y / self.xy_resolution < ny < self.map.max_y / self.xy_resolution)):
                     continue
 
-                next_idx = nx + ny * map_width_reso + nyaw * map_width_reso * map_height_reso
+                next_idx = self.map.get_grid3D_idx(nx, ny, nyaw, self.xy_resolution)
                 if next_idx in closed_set:
                     continue
 
                 new_cost = 1
-
                 if direction == -1:
                     new_cost *= reverse_penalty
-
                 if curr_dir != direction:
                     new_cost += change_direction_penalty
-
                 new_cost += abs(angle) * steering_penalty
-                new_cost += abs(curr_yaw - nyaw) * 2
+                new_cost += abs(goal_node.yaw - nyaw) * 2
                 new_cost += self.manhattan(goal_node, nx, ny)
 
                 if next_idx in open_set and open_set[next_idx] < new_cost:
@@ -336,7 +299,7 @@ class HybridAStar:
         open_set = dict()
         closed_set = dict()
 
-        open_set[self.map.get_grid_idx(goal_node.x, goal_node.y, self.xy_resolution)] = goal_node
+        open_set[self.map.get_grid2D_idx(goal_node.x, goal_node.y, self.xy_resolution)] = goal_node
         while open_set:
             curr_id = min(open_set, key=lambda o: open_set[o].cost)
             curr = open_set[curr_id]
@@ -351,7 +314,7 @@ class HybridAStar:
                                      curr.y + my,
                                      curr.cost + cost)
 
-                neighbor_id = self.map.get_grid_idx(neighbor_node.x, neighbor_node.y, self.xy_resolution)
+                neighbor_id = self.map.get_grid2D_idx(neighbor_node.x, neighbor_node.y, self.xy_resolution)
 
                 if not self.map.verify_node(neighbor_node.x, neighbor_node.y, self.xy_resolution):
                     continue
@@ -374,44 +337,80 @@ class HybridAStar:
         pass
 
 if __name__ == "__main__":
-    start = [10.0, 10.0, np.deg2rad(90.0)]
-    goal = [50.0, 50.0, np.deg2rad(0.0)]
+    start = [10.0, 10.0, np.deg2rad(0.0)]
+    goal = [55.0, 30.0, np.deg2rad(90.0)]
+    arrow_length = 2 * 0.5
+    dx = arrow_length * math.cos(goal[2])
+    dy = arrow_length * math.sin(goal[2])
+
+    plt.arrow(goal[0], goal[1], dx, dy,
+              head_width=0.3, head_length=0.4,
+              fc="blue", ec="blue")
     ioniq5 = Car(*start)
 
     ox, oy = [], []
 
+    # for i in range(60):
+    #     ox.append(i)
+    #     oy.append(0.0)
+    # for i in range(60):
+    #     ox.append(60.0)
+    #     oy.append(i)
+    # for i in range(61):
+    #     ox.append(i)
+    #     oy.append(60.0)
+    # for i in range(61):
+    #     ox.append(0.0)
+    #     oy.append(i)
+    # for i in range(40):
+    #     ox.append(20.0)
+    #     oy.append(i)
+    # for i in range(40):
+    #     ox.append(40.0)
+    #     oy.append(60.0 - i)
+
     for i in range(60):
         ox.append(i)
         oy.append(0.0)
-    for i in range(60):
+    for i in range(40):
         ox.append(60.0)
         oy.append(i)
     for i in range(61):
         ox.append(i)
-        oy.append(60.0)
-    for i in range(61):
+        oy.append(40.0)
+    for i in range(41):
         ox.append(0.0)
         oy.append(i)
-    for i in range(40):
-        ox.append(20.0)
+
+    for i in range(25):
+        ox.append(50.0)
         oy.append(i)
-    for i in range(40):
-        ox.append(40.0)
-        oy.append(60.0 - i)
+
+    for i in range(10):
+        ox.append(50.0 + i)
+        oy.append(25)
+
+    for i in range(10):
+        ox.append(50.0 + i)
+        oy.append(35)
+
+    for i in range(40 - 35):
+        ox.append(50.0)
+        oy.append(35 + i)
 
 
     grid_map = Map(ox, oy)
+    plt.pause(0.1)
 
     hybrid_a_star = HybridAStar(grid_map, ioniq5, start, goal, 2, np.deg2rad(15))
     r_x, r_y, r_yaw = hybrid_a_star.planning()
     plt.plot(r_x, r_y, "-r")
     print(r_x, r_y, r_yaw)
-    # plt.cla()
     for x, y, yaw in zip(r_x, r_y, r_yaw):
         ioniq5.x, ioniq5.y, ioniq5.yaw = x, y, yaw
         plt.cla()
         grid_map.draw()
         plt.plot(r_x, r_y, "-r")
         ioniq5.draw()
-        plt.pause(0.01)
+        plt.pause(0.1)
     plt.show()
