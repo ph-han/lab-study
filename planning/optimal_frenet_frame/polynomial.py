2aaac0ab6100089abfe4f1b8cc24258bb19e53f4
ph-han <hanfeelhoo@naver.com>
2025-08-29 16:26:33 +0900

    WIP: polynomial solver

--- planning/optimal_frenet_frame/polynomial.py
+++ planning/optimal_frenet_frame/polynomial.py
@@ -1,8 +1,15 @@
 import numpy as np
 
 class Quintic:
-    def __init__(self, d_0, dd_0, ddd_0, d_1, dd_1, ddd_1):
-        pass
+    def __init__(self, t, d_0, dd_0, ddd_0, d_1, dd_1, ddd_1):
+        self.t = t
+        self.c0 = d_0
+        self.c1 = dd_0
+        self.c2 = 2 * ddd_0
+
+        self.m1 = np.array([
+            [1, t, t]
+        ])
 
 class Quartic:
     def __init__(self):
