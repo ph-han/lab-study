7739ad067497786239c7a3d99e9cafc194e9ad1c
phan <hanfeelhoo@naver.com>
2025-08-30 18:49:03 +0900

    feat: quartic solver

--- planning/optimal_frenet_frame/polynomial.py
+++ planning/optimal_frenet_frame/polynomial.py
@@ -34,8 +34,35 @@ class Quintic:
         return 20 * self.c5 * (t ** 3) + 12 * self.c4 * (t ** 2) + 6 * self.c3 * t + 2 * self.c2
 
 class Quartic:
-    def __init__(self):
-        pass
+    def __init__(self, qi_0, qi_1, qi_2, qt_1, qt_2, tt):
+        self.qt = np.array([qt_1, qt_2])
+
+        self.c0 = qi_0
+        self.c1 = qi_1
+        self.c2 = qi_2 * 0.5
+
+        self.c012 = np.array([self.c0, self.c1])
+
+        self.m1 = np.array([
+            [1, 2 * tt],
+            [0, 2]
+        ])
+
+        self.m2 = np.array([
+            [3 * (tt ** 2), 4 * (tt ** 3)],
+            [6 * tt, 12 * (tt ** 2)]
+        ])
+
+        self.c3, self.c4 = np.linalg.inv(self.m2) @ (self.qt - (self.m1 @ self.c012))
+
+    def get_position(self, t):
+        return self.c4 * (t ** 4) + self.c3 * (t ** 3) + self.c2 * (t ** 2) + self.c1 * t + self.c0
+
+    def get_velocity(self, t):
+        return 4 * self.c4 * (t ** 3) + 3 * self.c3 * (t ** 2) + 2 * self.c2 * t + self.c1
+
+    def get_acceleration(self, t):
+        return 12 * self.c4 * (t ** 2) + 6 * self.c3 * t + 2 * self.c2
 
 if __name__ == "__main__":
     quintic = Quintic(0, 0, 0, 3, 2, 1, 2)
