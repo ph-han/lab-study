cc5c4aa337950898c87471395b67d5ea2fe612f7
phan <hanfeelhoo@naver.com>
2025-08-30 19:24:29 +0900

    feat: generate longitudinal movment & plot longitudinal trajectories

---
+++ planning/optimal_frenet_frame/config.py
@@ -0,0 +1,14 @@
+# lateral final state position config
+DT_0_MIN = 1
+DT_0_MAX = 3
+DT_0_STEP = 0.5
+
+# longitunial final state position config
+ST_1_MIN = 12
+ST_1_MAX = 20
+ST_1_STEP = 1
+
+# terminal time config
+TT_MIN = 1
+TT_MAX = 3
+TT_STEP = 0.5
