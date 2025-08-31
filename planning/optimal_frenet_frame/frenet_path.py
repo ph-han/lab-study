139dc6ef3deaf07a84eba013cd7f73b14ce9df3d
phan <hanfeelhoo@naver.com>
2025-08-31 16:06:16 +0900

    feat: define frenet path class

--- planning/optimal_frenet_frame/frenet_path.py
+++ planning/optimal_frenet_frame/frenet_path.py
@@ -2,4 +2,22 @@ import numpy as np
 
 class FrenetPath:
     def __init__(self):
-        pass
+        # time list
+        self.t = []
+
+        # lateral traj
+        self.d0 = [] # position
+        self.d1 = [] # velocity
+        self.d2 = [] # acceleration
+        self.dj = [] # jerk
+
+        # longitudinal traj
+        self.s0 = []  # position
+        self.s1 = []  # velocity
+        self.s2 = []  # acceleration
+        self.sj = []  # jerk
+
+        # cost
+        self.lat_cost = 0
+        self.lon_cost = 0
+        self.tot_cost = 0
