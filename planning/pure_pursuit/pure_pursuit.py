631434a483bbec53fb48e4b1f2ab1c55576e862d
ph-han <hanfeelhoo@naver.com>
2025-09-30 20:09:24 +0900

    control todo write

--- planning/pure_pursuit/pure_pursuit.py
+++ planning/pure_pursuit/pure_pursuit.py
@@ -1,11 +1,24 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def pid(curr, target):
-    pass
+def pid_accel_control(curr, target):
+    '''
+    TODO
+    
+    '''
 
-def pursuit(curr, target):
-    pass
+def pursuit_steer_control(curr, traj):
+    '''
+    TODO
+    curr = (position, yaw, velocity)
+    traj = list
+    
+    1. waypoint를 찾아 (lookahead distance 만큼 떨어진)
+    2. tan(theta) = L/R, R = Ld / (2 * sin(alpha)) (Ld: k_ld * velocity, alpha = atan2(y2 - y1, x2 - x1) - curr_yaw)
+    3. delta = atan2(2 * L * sin(alpha), Ld) ==> steer angle
+    4. limit steer angle
+    '''
+
 
 if __name__ == "__main__":
     center_line_xlist = np.linspace(10, 30, 100)
