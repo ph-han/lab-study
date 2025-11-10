186a0ced2d55af7e9d20f8841a5b33a59a021cc7
ph-han <hanfeelhoo@naver.com>
2025-11-10 10:52:50 +0900

    feat: RRT start

---
+++ planning/RRT/rrt.py
@@ -0,0 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+class RRT:
+    def __init__(self):
+        pass
+
+    def planning(self):
+        pass
+
+if __name__ == "__main__":
+    rrt = RRT()
+    rrt.planning()
